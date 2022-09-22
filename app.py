21f83bc028e4799d7f9fed0482f20ee07b5b1e3b
Masutier <masutier@gmail.com>
2022-09-21 21:49:36 -0500

    Add infura - Main net, Rinkeby net and Kovan net info

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 import json
-from flask import Flask, request, redirect, render_template as render
+from flask import Flask, request, redirect, flash, render_template as render
 from web3 import Web3
 from pathlib import Path
 
-app = Flask(__name__)
-
 with open('/home/gabriel/prog/json_config/wimm.json') as config_file:
     config = json.load(config_file)
+
+app = Flask(__name__)
+app.config.update(
+    TESTING = True,
+    SECRET_KEY = config["PRIVATE_KEY"]
+)
 
 
 @app.route('/')
@@ -24,22 +28,35 @@ def home():
     return render('home.html', **context)
 
 
+@app.route('/infuraPoint', methods=['GET', 'POST'])
 def infuraBlock():
-    infura_url = config['INFURA_URL']
-    web3 = Web3(Web3.HTTPProvider(infura_url))
-    balance = web3.eth.getBalance("0xFC58C26caf80ef110B4552C1AcCb0986de5142F3")
+    if request.method == "POST":
+        option = request.form.getlist('options')
 
-    print(web3.isConnected())
-    print(web3.eth.blockNumber)
-    print(web3.fromWei(balance, "ether"))
-    return
+        if option == ['option1']:
+            web3 = Web3(Web3.HTTPProvider(config['INFURA_URL']))
+            endpoint = "Infura Main Net"
 
+        elif option == ['option2']:
+            web3 = Web3(Web3.HTTPProvider(config['KOVAN_URL']))
+            endpoint = "Infura Kovan Net"
 
-def ganacheBlock():
-    ganache_url = config['GANACHE_URL']
-    web3 = Web3(Web3.HTTPProvider(ganache_url))
-    print(web3.isConnected())
-    return
+        elif option == ['option3']:
+            web3 = Web3(Web3.HTTPProvider(config['RINKEBY_URL']))
+            endpoint = "Infura Rinkeby Net"
+
+        else:
+            flash("Something went wrong, please try again")
+            return redirect('infuraPoint.html')
+
+        balance = web3.eth.getBalance("0xFC58C26caf80ef110B4552C1AcCb0986de5142F3")
+        blockNumber = web3.eth.blockNumber
+        weiBalance = web3.fromWei(balance, "ether")
+
+        context = {"balance":balance, "blockNumber":blockNumber, "weiBalance":weiBalance, "endpoint":endpoint}
+        return render('displayNet.html', **context)
+
+    return render('infuraPoint.html')
 
 
 @app.route('/read_transaction', methods=['GET', 'POST'])
